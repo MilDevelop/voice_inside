8d1f732b078a45fee15738ec1b2911ad5091478a
BaCat8 <andreyka_l08@mail.ru>
2024-05-02 16:16:19 +0700

    Add fight

---
+++ fight test.py
@@ -0,0 +1,60 @@
+ class Enemy:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.attack = 10
+
+    def take_damage(self, damage):  # Дописать и использовать
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.attack = 15
+
+    def f_menu(self, enemy_hp):
+        print("Выберите действие:", "1) Атака", "2) Ничего", sep='\n')
+        answer = int(input())
+        if answer == 1:
+            print(enemy_hp)
+            print(self.attack)
+            enemy_hp -= self.attack
+        else:
+            pass
+
+    def take_damage(self, damage):  # Дописать и использовать 
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+
+class Master:
+    @staticmethod
+    def fight(player: Player, enemy: Enemy):
+        tick = 2
+        while tick != 0:
+            if player.health > 0 and enemy.health > 0:
+                if tick % 2 == 0:
+                    player.f_menu(enemy.health)
+                else:
+                    player.take_damage(enemy.attack)
+                tick += 1
+            else:
+                if player.health <= 0:
+                    print("game over")
+                elif enemy.health <= 0:
+                    print("The opponent lost ")
+                tick = 0
+            # print(player.health)
+            # print(enemy.health)
+
+
+player1 = Player("Маникен")
+enemy1 = Enemy("Тестовый враг")
+master = Master
+master.fight(player1, enemy1)
+
