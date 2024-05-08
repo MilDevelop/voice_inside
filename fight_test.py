9a9e6e64f9578d77a171cbfe19af9016d1adf8b9
BaCat8 <andreyka_l08@mail.ru>
2024-05-08 17:50:01 +0700

    Классы врагов, начало работы над локациями

--- fight_test.py
+++ fight_test.py
@@ -1,16 +1,44 @@
-class Enemy:
-    def __init__(self, name, hp=100, att=10):
-        self.name = name
-        self.health = hp
-        self.attack = att
+# Сделать логирование
 
-    def take_damage(self, damage):  # Дописать и использовать
+class Location: # Сделать, доработать
+    def __init__(self, name):
+        self.name = name
+        self.places = {}
+        self.connected_locations = {}
+        self.npcs = {"enemys": []}  # НаВеРнОе сделать словарём
+    def exit():
+        pass
+        
+
+# class LocaGlobal(Location):
+#     def __init__(self, name):
+#         super().__init__(name)
+#         self.connected_locations = {}
+#     def main():
+#         pass
+
+
+# class LocaLoc(Location):
+#     def main():
+#         pass
+
+
+class Enemy:
+    def __init__(self, name):
+        self.name = name
+        
+
+    def take_damage(self, damage):
         self.health -= damage
         if self.health < 0:
             self.health = 0
 
 
-class Clot(Enemy("Сlot", 15, 1)):
+class Clot(Enemy):
+    def __init__(self, hp=50, att=1):
+        super().__init__("Сlot")
+        self.health = hp
+        self.attack = att
     def main(self):
         pass
 
@@ -47,7 +75,7 @@ class Player:
 class Master:
 
     @staticmethod
-    def fight(player: Player, enemy: Enemy):  # Здесь какая-то ошибка...
+    def fight(player: Player, enemy: Enemy):
         tick = 2
         while tick != 0:
             if player.health > 0 and enemy.health > 0:
@@ -55,19 +83,21 @@ class Master:
                     print("Выберите действие:", "1) Атака", "2) Ничего", sep='\n')
                     answer = int(input())
                     if answer == 1:
-                        print(enemy.health)
-                        print(player.attack)
+                        print(f"Здоровье врага: {enemy.health}")
+                        print(f"Здоровье игрока: {player.health}")
+                        print(f"Игрок атакует с силой равной {player.attack}")
                         enemy.take_damage(player.attack)
                     else:
                         pass
                 else:
+                    print(f"Противник атакует с силой равной {enemy.attack}")
                     player.take_damage(enemy.attack)
                 tick += 1
             else:
                 if player.health <= 0:
-                    print("game over")
+                    print("Поражение маникена")
                 elif enemy.health <= 0:
-                    print("The opponent lost ")
+                    print("Противник погиб")
                 tick = 0
             # print(player.health)
             # print(enemy.health)
@@ -84,6 +114,9 @@ class Master:
 
 
 player1 = Player("Маникен")
-enemy1 = Clot
-master = Master
+enemy1 = Clot()
+master = Master()
+print(enemy1.name)
 master.fight(player1, enemy1)
+
+# Обновление кода от 08.05 на паре
